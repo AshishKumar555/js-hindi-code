7f01d9044de0e7e4c3392240c859af3d9fbbf047
AshishKumar555 <112402883+AshishKumar555@users.noreply.github.com>
2023-12-26 08:17:51 +0530

    comment added

--- 06_advance/40_promise.js
+++ 06_advance/40_promise.js
@@ -1,9 +1,9 @@
 const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
     // DB calls, cryptography, network
-    setTimeout(function(){
+    setTimeout(function(){      // function jo given time ke bad execute hota hai
         console.log('Async task is compelete');
-        resolve()
+        resolve()               //resolve connect .then 
     }, 1000)
 })
 
@@ -11,6 +11,7 @@ promiseOne.then(function(){
     console.log("Promise consumed");
 })
 
+//  Promise 2 ---------------------------------
 new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("Async task 2");
@@ -21,6 +22,7 @@ new Promise(function(resolve, reject){
     console.log("Async 2 resolved");
 })
 
+//-------------------------------------------------------------------***
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
         resolve({username: "Chai", email: "chai@example.com"})
@@ -31,6 +33,7 @@ promiseThree.then(function(user){
     console.log(user);
 })
 
+//--------------------------------------------------------------***
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
         let error = true
